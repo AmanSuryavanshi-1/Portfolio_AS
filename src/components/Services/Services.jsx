fdc6d4196a43a20448ee9e2490134f1dae903019
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2023-07-31 14:19:24 +0530

    31/7

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Services.css'
-
+import Img1 from '../../Assests/service.svg'
+import Img2 from '../../Assests/service-2.svg'
+import Img3 from '../../Assests/service-3.svg'
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="Services">
+        <h2>Services</h2>
+      <div className="container services_container">
+        <div className="services_card">
+          <img src={Img1} alt="" className='services_img'/>
+          <h2>UI/UX Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci obcaecati dolorem magnam nesciunt corporis laboriosam labore cumque nihil similique architecto eaque, tempora repellat minima nobis.</p>
+        </div>
+
+        <div className="services_card">
+          <img src={Img2} alt="" className='services_img'/>
+          <h2>Web Development</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci obcaecati dolorem magnam nesciunt corporis laboriosam labore cumque nihil similique architecto eaque, tempora repellat minima nobis.</p>
+        </div>
+
+        <div className="services_card">
+          <img src={Img3} alt="" className='services_img'/>
+          <h2>Software Development</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci obcaecati dolorem magnam nesciunt corporis laboriosam labore cumque nihil similique architecto eaque, tempora repellat minima nobis.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
