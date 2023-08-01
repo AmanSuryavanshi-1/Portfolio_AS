34eb015888b9caf7dc8b0ac315842497a0157067
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2023-08-02 02:28:44 +0530

    2/8

---
+++ src/components/Portfolio/Data.jsx
@@ -0,0 +1,109 @@
+import work1 from "../../Assests/work-1.svg"
+import work2 from "../../Assests/work-2.svg"
+import work3 from "../../Assests/work-3.svg"
+import work4 from "../../Assests/work-4.svg"
+import work5 from "../../Assests/work-5.svg"
+import work6 from "../../Assests/work-6.svg"
+
+const Data =[
+{
+    id: 1,
+    image: work1,
+    title: "E-commerce App",
+    link: "https://ase-commerce.netlify.app/",
+    github_link: "https://github.com/AmanSuryavanshi-1/E-commerce-App",
+    // description:"Created a responsive React-based E-commerce clone with product listings, shopping categories, and a user-friendly shopping cart.Committed to delivering a seamless shopping experience for users. Utilized JSX, ReactJs, and MaterialUI for a polished and efficient web app.",
+    category: "Major",
+},
+{
+    id: 2,
+    image: work2,
+    title: "NewsApp",
+    link: "",
+    github_link: "https://github.com/AmanSuryavanshi-1/NewsApp",
+    category: "Major",
+},
+{
+    id: 3,
+    image: work3,
+    link: "https://cloud-note-two.vercel.app/",
+    github_link: "https://github.com/AmanSuryavanshi-1/CloudNote",
+    title: "CloudNote",
+    category: "Major",
+},
+{
+    id: 4,
+    image: work4,
+    link: "https://as-textutils.netlify.app/",
+    github_link: "https://github.com/AmanSuryavanshi-1/TextUtils",
+    title: "TextUtils",
+    category: "Major",
+},
+{
+    id: 5,
+    image: work4,
+    link: "",
+    github_link: "https://github.com/AmanSuryavanshi-1/CovidTracker",
+    title: "CovidTracker",
+    category: "Major",
+},
+{
+    id: 6,
+    image: work6,
+    link: "https://amansuryavanshi-1.github.io/Spotify-Clone/",
+    github_link: "https://github.com/AmanSuryavanshi-1/Spotify-Clone",
+    title: "SpotifyClone",
+    category: "Minor",
+},
+{
+    id: 7,
+    image: work5,
+    link: "https://amansuryavanshi-1.github.io/Analog-Clock/",
+    github_link: "https://github.com/AmanSuryavanshi-1/Analog-Clock",
+    title: "Analog Clock",
+    category: "Minor",
+},
+{
+    id: 8,
+    image: work5,
+    link: "https://amansuryavanshi-1.github.io/Digital-Clock/",
+    github_link: "https://github.com/AmanSuryavanshi-1/Digital-Clock",
+    title: "Digital Clock",
+    category: "Minor",
+},
+{
+    id: 9,
+    image: work6,
+    link: "https://amansuryavanshi-1.github.io/Notes-Keeper-/",
+    github_link: "https://github.com/AmanSuryavanshi-1/Notes-Keeper-",
+    title: "Notes Keeper",
+    category: "Minor",
+},
+{
+    id: 10,
+    image: work6,
+    link: "https://amansuryavanshi-1.github.io/QR-Code-Generator/",
+    github_link: "https://github.com/AmanSuryavanshi-1/QR-Code-Generator",
+    title: "QR Code Generator",
+    category: "Minor",
+},
+{
+    id: 11,
+    image: work1,
+    title: "Calm Sleep Dashboard",
+    link: "https://calm-sleep-dashboard.netlify.app/",
+    github_link: "https://github.com/AmanSuryavanshi-1/Calm-Sleep-Assignment",
+    // description:"Created a responsive React-based E-commerce clone with product listings, shopping categories, and a user-friendly shopping cart.Committed to delivering a seamless shopping experience for users. Utilized JSX, ReactJs, and MaterialUI for a polished and efficient web app.",
+    category: "Minor",
+},
+{
+    id: 12,
+    image: work6,
+    link: "",
+    github_link: "",
+    title: "Rotating Navbar",
+    category: "Minor",
+}
+]
+
+export default Data
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,61 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './Portfolio.css'
+import Data from './Data'
+import { useState } from 'react';
+import {FiLink} from 'react-icons/fi'
+import {BiCodeAlt} from 'react-icons/bi'
+
 const Portfolio = () => {
+  const [items, setItems] = useState(Data);
+  const filterItem = (categoryItem) =>{
+    const updatedItems = Data.filter((curElem) =>{
+      return curElem.category === categoryItem;
+    });
+
+    setItems(updatedItems);
+  }
+
   return (
-    <section id='Portfolio'>Portfolio</section>
+    <section id='Portfolio'>
+      <h2>Recent Works</h2>
+      <div className='container portfolio_container'>
+        <div className='work_filters'>
+          <span className="work_item" onClick={() => setItems(Data)} >Everything</span>
+          <span className="work_item" onClick={() => filterItem("Major")}>Major Projects </span>
+          <span className="work_item" onClick={() => filterItem("Minor")}> Minor Projects </span>
+        </div>
+
+        <div className="work_container">
+          {items.map((elem) =>{
+            const{id, image, title,link, github_link, category} = elem;
+            return (
+              <div className="work_card" key={id}>
+                <div className="work_thumbnail">
+                  <img src={image} alt="" className='work_img'/>
+                  <div className='work_mask'></div>
+                </div>
+
+                <span className="work_category">{category}</span>
+                <h3 className="work_title">{title}</h3>
+                {/* <p className="work_category">{description}</p> */}
+                {link && (
+                <a href={link} className='link_button'>
+                  <i className="icon-link work_button-icon"><FiLink/></i>
+                </a>
+                )}
+
+{github_link && (
+                <a href={github_link} className='code_button'>
+                <i className="icon-link work_button-icon"><BiCodeAlt/></i>
+                </a>
+)}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
